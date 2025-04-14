e07a4e2bdf3d6f5ac734f901fc0fa0235e524aa6
Arthur Nogaroto <a.h.nogaroto@gmail.com>
2025-04-14 16:37:44 -0300

    Atualiza estrutura das playlists e adiciona componente Grade

--- src/mocks/playlists.js
+++ src/mocks/playlists.js
@@ -1,3 +1,7 @@
+import liked from "../../assets/playlists/liked.png"
+
 const playlists = [
     { id: '1', nome: 'Liked Songs', imagem: liked}
 ]
+
+export default playlists;
---
+++ src/telas/components/grade.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+import playlists from "../../mocks/playlists";
+
+console.log (playlists);
