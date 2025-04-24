549f41e7ce62bcf5b402290079e63016d8144958
Arthur Nogaroto <a.h.nogaroto@gmail.com>
2025-04-24 07:31:59 -0300

    Criação de uma parte de grade, com playlists e botões.

---
+++ src/mocks/playlistsNames.js
@@ -0,0 +1,22 @@
+import liked from "../../assets/playlists/liked.png";
+import blonde from "../../assets/playlists/blonde.png";
+import tpab from "../../assets/playlists/tpab.png";
+import auto from "../../assets/playlists/ad.png";
+import clube from "../../assets/playlists/clube.png";
+import charm from "../../assets/playlists/charm.png";
+import tabua from "../../assets/playlists/atde.png";
+import music from "../../assets/playlists/music.png";
+
+const playlists = [
+    { id: '1', nome: 'Liked Songs', imagem: liked},
+    { id: '2', nome: 'Blonde', imagem: blonde},
+    { id: '3', nome: 'To Pimp A Butterfly', imagem: tpab},
+    { id: '4', nome: 'Autodomínio', imagem: auto},
+    { id: '5', nome: 'Clube da Esquina', imagem: clube},
+    { id: '6', nome: 'Charm', imagem: charm},
+    { id: '7', nome: 'A Tabua De Esmeralda', imagem: tabua},
+    { id: '8', nome: 'Music', imagem: music},
+
+]
+
+export default playlists;
